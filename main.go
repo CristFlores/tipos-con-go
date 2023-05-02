dd27c5623d03c1636bed6fb9682015c2d7af07e4
Cristian Flores <carmando.fa05@gmail.com>
2023-05-01 23:30:56 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type course struct {
+	name string
+}
+
+func (c course) Print() {
+	fmt.Printf("%+v\n", c)
+}
+
+func main() {
+	c := course{name: "Go"}
+	c.Print()
+}
